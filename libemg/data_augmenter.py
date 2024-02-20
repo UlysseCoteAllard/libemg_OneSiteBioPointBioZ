76c57463f8a9dc7966e7d1db02624c5c31b6a676
ECEEvanCampbell <evan.campbell1@unb.ca>
2024-02-20 15:24:26 -0400

    added 60hz and MA noise

--- libemg/data_augmenter.py
+++ libemg/data_augmenter.py
@@ -1,6 +1,6 @@
 import numpy as np
 import random
-from scipy import interpolate
+from scipy import interpolate, signal
 from sktime.transformations.panel.interpolate import TSInterpolator
 
 class DataAugmenter:
@@ -25,10 +25,12 @@ class DataAugmenter:
                 'POL',          # Flips the polarity of the signal
                 'ZERO',         # Randomly zeros values
                 'MIXUP',        # Randomly mixes up the array
-                ]  
+                '60HZ',         # 60 Hz noise  
+                "MA",           # Motion Artefact
+                ]         
 
     def augMIXUP(self, data, percent=0.05, mixups=5):
-        for _ in range(0, 5):
+        for _ in range(0, mixups):
             start_idx = random.randint(1, len(data) - len(data) * percent)
             data = np.vstack([data[0:start_idx,:], data[int(start_idx+len(data)*percent):len(data)], data[start_idx:int(start_idx+len(data)*percent):,:] ])
         return data 
@@ -79,3 +81,24 @@ class DataAugmenter:
 
     def augFLIP(self, data):
         return np.flip(data, axis=0)
+    
+    def aug60HZ(self, data, snr=3, fs=1000):
+        signal_power = 20*np.log10(np.mean(np.abs(data)))
+        noise_power  = -1*(snr - signal_power)
+        noise_amplitude = np.power(10, noise_power/20)
+        t = np.arange(data.shape[0])
+        noise = noise_amplitude*np.sin(2*np.pi * 60/fs* t)
+        return data + np.expand_dims(noise, 1)
+    
+    def augMA(self, data, snr=3, dc=0.3, fs=1000):
+        signal_power = 20*np.log10(np.mean(np.abs(data)))
+        noise_power  = -1*(snr - signal_power)
+        noise_amplitude = np.power(10, noise_power/20)
+        t = np.arange(data.shape[0])
+        duty_cycle_signal = signal.square(t, duty=dc)
+        duty_cycle_signal = duty_cycle_signal == 1
+        noise = np.zeros_like(t, dtype=np.float32)
+        for i in range(-10,10):
+            noise += (noise_amplitude+-1*np.abs(i)/10) * np.sin(2*np.pi * (i+10)/fs* t)
+        noise = noise * duty_cycle_signal
+        return data + np.expand_dims(noise, 1)
--- test_augmenter.py
+++ test_augmenter.py
@@ -6,9 +6,11 @@ da = DataAugmenter()
 
 data = np.loadtxt("R_0_C_0.csv", delimiter=',')
 
-data2 = da.augMIXUP(data)
+# data2 = da.aug60HZ(data, 0, 200)
+data3 = da.augMA(data, 0, 0.3, 200)
+# data2 = da.augMIXUP(data)
 
 fig, axs = plt.subplots(2)
 axs[0].plot(data)
-axs[1].plot(data2)
+axs[1].plot(data3)
 plt.show()
